961141a3d6b1ba4f76538d9cabddf0e7af9d9ed7
JdM <julien@rmbi.ch>
2023-05-20 17:05:51 +0200

    Message for window system

--- components/appIcon/index.js
+++ components/appIcon/index.js
@@ -1,20 +1,41 @@
-import styles from "./appIcon.module.css"
+import styles from "./appIcon.module.css";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-function handleClick(){
-    
-}
+export default function AppIcon(props) {
+  const [isLaunched, setIsLaunched] = useState(false);
 
-export default function AppIcon(props){
-    const [isLaunched, setIsLaunched] = useState(0)
+  const imagesPath = props.imgSrc;
+  const app_name = props.appName;
+  const window = props.Window;
 
-    const imagesPath = props.imgSrc
-    const app_name = props.appName
-    const window = props.Window
-    return(
-        <div className={styles.appDiv}>
-            <Image src={imagesPath} onClick={handleClick} className={styles.appImg} alt={app_name} width={40} height={40} />
-        </div>
-    )
+  function HandleClick() {
+    setIsLaunched(true);
+  }
+
+  function displayWindow(isLaunched) {
+    if (isLaunched) {
+      return (window());
+    }
+  }
+
+  useEffect(() => {
+    // Any side effects can be performed here when `isLaunched` changes
+  }, [isLaunched]);
+
+  return (
+    <>
+      <div className={styles.appDiv}>
+        <Image
+          src={imagesPath}
+          onClick={HandleClick}
+          className={styles.appImg}
+          alt={app_name}
+          width={40}
+          height={40}
+        />
+      </div>
+      {displayWindow(isLaunched)} {/* Call the displayWindow function */}
+    </>
+  );
 }
